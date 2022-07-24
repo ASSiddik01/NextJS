607c6ef50b77eaab69f411a7da5b048208cadcdc
Shama <assiddik001@gmail.com>
2022-07-24 18:20:12 +0600

    Image Component

--- my_next_app/pages/blog.js
+++ my_next_app/pages/blog.js
@@ -1,9 +1,12 @@
 import React from "react";
+import codaing from "../image/coading.jpg";
+import Image from "next/image";
 
 const Blog = () => {
   return (
     <div>
       <h2>Blog</h2>
+      <Image src={codaing} alt="" width={700} height={400} />
     </div>
   );
 };
